c96dafc4e7254f445bfc0db3f3996078ba1abff0
Emily Liu <emilylau211@gmail.com>
2021-10-02 13:44:35 -0700

    Time: 80 ms (62.71%), Space: 40.8 MB (37.89%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,12 @@
+
+var reverseList = function(head) {
+    let prev = null;
+    let next = null;
+    while(head){
+        next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+};
