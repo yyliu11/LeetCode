fe93887aa8808c7eb47ddf0e42ea27edbd3b5c1e
Emily Liu <emilylau211@gmail.com>
2021-10-09 22:04:41 -0700

    Time: 100 ms (85.43%), Space: 43.9 MB (66.06%) - LeetHub

---
+++ merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+// var mergeKLists = function(lists) {
+//     if(!lists.length){ return []; }
+//     let array = [];
+    
+//     const getValue = (node)=>{
+//         if(!node){ return; }
+//         array.push(node.val);
+//         getValue(node.next)
+//     }; 
+//     for(let list of lists){
+//        getValue(list) 
+//     }
+//     array.sort((a,b)=>a-b);
+//     let head = new ListNode(array[0]);
+//     let node = head;
+//     for(let i = 1; i < array.length; i++){
+//         node.next = new ListNode(array[i]);
+//         node = node.next;
+//     }
+//     return head;
+// };
+var mergeKLists = function(lists) {
+   let arrayOutput = [];
+    const pushLinkedListValue = (node) => {
+        if(!node){return}
+        arrayOutput.push(node.val);
+        return pushLinkedListValue(node.next);
+    }
+    for(let i=0; i<lists.length; i++){
+        pushLinkedListValue(lists[i]);
+    }
+    arrayOutput.sort(function(a, b) {
+      return a - b;
+    });
+    let head = null;
+    let newlinked;
+    for(let i=0; i<arrayOutput.length;i++){
+        if(i==0){
+            head = new ListNode(arrayOutput[i],null);
+            newlinked = head;
+        }
+        else{
+            newlinked.next = new ListNode(arrayOutput[i],null);
+            newlinked = newlinked.next;
+        }
+    }
+    return head;
+    
+}
