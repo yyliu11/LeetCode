1b20010ecd7fe51f81167df004185da25d434b29
Emily Liu <emilylau211@gmail.com>
2021-10-09 22:21:16 -0700

    Time: 88 ms (98.78%), Space: 44.1 MB (51.60%) - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.js
+++ merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -9,52 +9,57 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-// var mergeKLists = function(lists) {
-//     if(!lists.length){ return []; }
-//     let array = [];
-    
-//     const getValue = (node)=>{
-//         if(!node){ return; }
-//         array.push(node.val);
-//         getValue(node.next)
-//     }; 
-//     for(let list of lists){
-//        getValue(list) 
-//     }
-//     array.sort((a,b)=>a-b);
-//     let head = new ListNode(array[0]);
-//     let node = head;
-//     for(let i = 1; i < array.length; i++){
-//         node.next = new ListNode(array[i]);
-//         node = node.next;
-//     }
-//     return head;
-// };
 var mergeKLists = function(lists) {
-   let arrayOutput = [];
-    const pushLinkedListValue = (node) => {
-        if(!node){return}
-        arrayOutput.push(node.val);
-        return pushLinkedListValue(node.next);
+    let array = [];
+    
+    const getValue = (node)=>{
+        if(!node){ return; }
+        array.push(node.val);
+        return getValue(node.next)
+    }; 
+    for(let list of lists){
+       getValue(list) 
     }
-    for(let i=0; i<lists.length; i++){
-        pushLinkedListValue(lists[i]);
-    }
-    arrayOutput.sort(function(a, b) {
-      return a - b;
-    });
+    array.sort((a,b)=> a-b);
+   
     let head = null;
-    let newlinked;
-    for(let i=0; i<arrayOutput.length;i++){
-        if(i==0){
-            head = new ListNode(arrayOutput[i],null);
-            newlinked = head;
-        }
-        else{
-            newlinked.next = new ListNode(arrayOutput[i],null);
-            newlinked = newlinked.next;
-        }
+    let node;
+    for(let i = 0; i < array.length; i++){
+        if(i === 0){
+            head = new ListNode(array[0]);
+            node = head;
+        }else{
+              node.next = new ListNode(array[i]);
+              node = node.next;
+        }  
     }
     return head;
+};
+// var mergeKLists = function(lists) {
+//    let arrayOutput = [];
+//     const pushLinkedListValue = (node) => {
+//         if(!node){return}
+//         arrayOutput.push(node.val);
+//         return pushLinkedListValue(node.next);
+//     }
+//     for(let i=0; i<lists.length; i++){
+//         pushLinkedListValue(lists[i]);
+//     }
+//     arrayOutput.sort(function(a, b) {
+//       return a - b;
+//     });
+//     let head = null;
+//     let newlinked;
+//     for(let i=0; i<arrayOutput.length;i++){
+//         if(i==0){
+//             head = new ListNode(arrayOutput[i],null);
+//             newlinked = head;
+//         }
+//         else{
+//             newlinked.next = new ListNode(arrayOutput[i],null);
+//             newlinked = newlinked.next;
+//         }
+//     }
+//     return head;
     
-}
+// }
