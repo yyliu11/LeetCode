c4f0715420a303e71dbf1fbd547ca10d5fbb7d17
Emily Liu <emilylau211@gmail.com>
2021-10-23 19:10:51 -0700

    Time: 112 ms (64.81%), Space: 47.1 MB (30.38%) - LeetHub

--- reorder-list/reorder-list.js
+++ reorder-list/reorder-list.js
@@ -9,40 +9,42 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
+const findMid =(h)=>{
+    let fast = h;
+    let slow = h;
+    while(fast&&fast.next){
+        fast=fast.next.next;
+        slow=slow.next
+    }
+    return slow;
+};
+
+const reverse = (list)=>{
+    let newNode = null;
+    while(list){
+        let next = list.next;
+        list.next = newNode;
+        newNode = list;
+        list = next
+    }
+    return newNode;
+};
+
 var reorderList = function(head) {
-    if(!head) return null;
+    if(!head){ return null; }
+    let l1 = head;
+    let mid = findMid(head);
+    let l2 = mid.next;
+    mid.next = null;
+    l2 = reverse(l2);
     
-    let slow = head;
-    let fast = head;
-    
-    // find the middle node
-    while(fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    
-    let prev = null;
-    let curr = slow;
-    
-    //reverse the 2nd half of linked list
-    while(curr) {
-        let temp = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = temp;
-    }
-    
-    // get the heads of two lists and merge
-    let first = head;
-    let second = prev;
-    
-    while(second.next) {
-        let temp = first.next;
-        first.next = second;
-        first = temp;
-        
-        temp = second.next;
-        second.next = first;
-        second = temp;
+    while(l1&&l2){
+      
+        let temp = l1.next;
+        l1.next = l2;
+        l2 = l2.next;
+        l1.next.next = temp;
+        l1 = l1.next.next;
     }
 };
+
