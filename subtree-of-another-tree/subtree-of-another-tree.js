5bd3223dfc08e0548853892bbda6a8cfad2d7c20
Emily Liu <emilylau211@gmail.com>
2021-11-18 22:33:42 -0800

    Time: 384 ms (5.42%), Space: 46.1 MB (11.69%) - LeetHub

---
+++ subtree-of-another-tree/subtree-of-another-tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if(!root)return false;
+    if(JSON.stringify(root) === JSON.stringify(subRoot))return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+};
